65d7e95ade6178fbf2854fe0106723a10d303175
Richmond Starbuck <rbstarbuck@gmail.com>
2024-10-22 02:54:01 -0400

    Layout and align board composables

--- app/src/main/java/com/example/worditory/game/board/BoardView.kt
+++ app/src/main/java/com/example/worditory/game/board/BoardView.kt
@@ -1,7 +1,21 @@
 package com.example.worditory.game.board
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.worditory.game.board.tile.TileView
 
 @Composable
 fun BoardView(viewModel: BoardViewModel) {
+    val flatTiles = viewModel.tiles.flatten()
+    LazyVerticalGrid(GridCells.Fixed(viewModel.width)) {
+        items(flatTiles.size) { i ->
+            Box(Modifier.aspectRatio(1f)) {
+                TileView(flatTiles[i])
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/worditory/game/board/tile/TileView.kt
+++ app/src/main/java/com/example/worditory/game/board/tile/TileView.kt
@@ -1,11 +1,22 @@
 package com.example.worditory.game.board.tile
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun TileView(viewModel: TileViewModel) {
     val letter = viewModel.letter.collectAsState("")
-    Text(letter.value)
+    Text(
+        text = letter.value,
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentHeight(Alignment.CenterVertically)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
