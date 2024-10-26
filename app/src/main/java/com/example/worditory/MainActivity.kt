da961e4f82bcc1527294799917784b77e32baec2
Richmond Starbuck <rbstarbuck@gmail.com>
2024-10-25 21:40:39 -0400

    Add test MainActivity

--- app/src/main/java/com/example/worditory/MainActivity.kt
+++ app/src/main/java/com/example/worditory/MainActivity.kt
@@ -4,44 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.worditory.game.WordDictionary
+import com.example.worditory.game.board.BoardView
+import com.example.worditory.game.board.BoardViewModel
 import com.example.worditory.ui.theme.WorditoryTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WorditoryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WordDictionary.Companion.init()
+
+                val width = 6
+                val height = 6
+                val board = BoardViewModel(width, height)
+
+                BoardView(board)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WorditoryTheme {
-        Greeting("Android")
     }
 }
