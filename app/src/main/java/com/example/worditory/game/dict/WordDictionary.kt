c3b17851231d1f0d76a61a330ac8c0c22b808619
Richmond Starbuck <rbstarbuck@gmail.com>
2024-10-27 19:11:35 -0400

    Convert WordDictionary to object instead of companion object

--- app/src/main/java/com/example/worditory/game/dict/WordDictionary.kt
+++ app/src/main/java/com/example/worditory/game/dict/WordDictionary.kt
@@ -3,7 +3,63 @@ package com.example.worditory.game.dict
 import java.io.BufferedReader
 import kotlin.math.absoluteValue
 
-class WordDictionary private constructor() {
+object WordDictionary {
+    private val wordFrequencies: List<WordFrequency>
+    private val wordSet: Set<String>
+
+    init {
+        val frequencies = mutableListOf<WordFrequency>()
+        val set = mutableSetOf<String>()
+
+        val reader =
+            this::class.java
+                .getResourceAsStream("/assets/dictionary.txt")
+                ?.bufferedReader() as BufferedReader
+        val lines = reader.readLines()
+        reader.close()
+
+        lines.forEach { line ->
+            val parts = line.split(",")
+            val word = parts[0]
+            val freq = parts[1].toInt()
+            frequencies.add(WordFrequency(word, freq))
+            set.add(word)
+        }
+
+        wordFrequencies = frequencies
+        wordSet = set
+    }
+
+    val size: Int
+        get() = wordFrequencies.size
+
+    fun init() = Unit
+
+    fun contains(word: String) = wordSet.contains(word)
+
+    fun search(word: String, previousResult: SearchResult): SearchResult {
+        var frequency = -1
+
+        val from = wordFrequencies.binarySearch(
+            previousResult.from.absoluteValue,
+            previousResult.to.absoluteValue
+        ) {
+            it.word.compareTo(word)
+        }
+
+        if (from > 0) frequency = wordFrequencies[from].frequency
+
+        val wordTail = "$word~"
+        val to = wordFrequencies.binarySearch(
+            previousResult.from.absoluteValue,
+            previousResult.to.absoluteValue
+        ) {
+            it.word.compareTo(wordTail)
+        }
+
+        return SearchResult(from, to, frequency)
+    }
+
     private data class WordFrequency(val word: String, val frequency: Int)
 
     data class SearchResult(
@@ -16,63 +72,5 @@ class WordDictionary private constructor() {
 
         val isPrefix
             get() =  from.absoluteValue != to.absoluteValue
-    }
-
-    companion object {
-        private val wordFrequencies: List<WordFrequency>
-        private val wordSet: Set<String>
-
-        init {
-            val frequencies = mutableListOf<WordFrequency>()
-            val set = mutableSetOf<String>()
-
-            val reader =
-                this::class.java
-                    .getResourceAsStream("/assets/dictionary.txt")
-                    ?.bufferedReader() as BufferedReader
-            val lines = reader.readLines()
-            reader.close()
-
-            lines.forEach { line ->
-                val parts = line.split(",")
-                val word = parts[0]
-                val freq = parts[1].toInt()
-                frequencies.add(WordFrequency(word, freq))
-                set.add(word)
-            }
-
-            wordFrequencies = frequencies
-            wordSet = set
-        }
-
-        val size: Int
-            get() = wordFrequencies.size
-
-        fun init() = Unit
-
-        fun contains(word: String) = wordSet.contains(word)
-
-        fun search(word: String, previousResult: SearchResult): SearchResult {
-            var frequency = -1
-
-            val from = wordFrequencies.binarySearch(
-                previousResult.from.absoluteValue,
-                previousResult.to.absoluteValue
-            ) {
-                it.word.compareTo(word)
-            }
-
-            if (from > 0) frequency = wordFrequencies[from].frequency
-
-            val wordTail = "$word~"
-            val to = wordFrequencies.binarySearch(
-                previousResult.from.absoluteValue,
-                previousResult.to.absoluteValue
-            ) {
-                it.word.compareTo(wordTail)
-            }
-
-            return SearchResult(from, to, frequency)
-        }
     }
 }
