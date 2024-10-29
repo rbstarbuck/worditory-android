fd956c65551f0765dc56c2583cd64c949eb004c9
Richmond Starbuck <rbstarbuck@gmail.com>
2024-10-28 22:47:31 -0400

    Round off word line

--- app/src/main/java/com/example/worditory/game/board/word/WordView.kt
+++ app/src/main/java/com/example/worditory/game/board/word/WordView.kt
@@ -1,13 +1,9 @@
 package com.example.worditory.game.board.word
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
@@ -15,6 +11,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.translate
 import com.example.worditory.game.board.tile.TileViewModel
@@ -57,7 +54,11 @@ fun WordView(viewModel: WordViewModel) {
                     path = path,
                     color = Color.Red,
                     alpha = 0.4f,
-                    style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
+                    style = Stroke(
+                        width = strokeWidth,
+                        cap = StrokeCap.Round,
+                        join = StrokeJoin.Round
+                    )
                 )
             }
         }
