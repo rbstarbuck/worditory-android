89c79e40eccf5e9a3d70bc5520078a36458078ee
Richmond Starbuck <rbstarbuck@gmail.com>
2024-11-03 21:44:18 -0500

    Fix bug where NPC didn't play first on loading saved game where it's NPC's turn

--- app/src/main/java/com/example/worditory/game/GameAgainstNpcViewModel.kt
+++ app/src/main/java/com/example/worditory/game/GameAgainstNpcViewModel.kt
@@ -12,31 +12,40 @@ class GameAgainstNpcViewModel(
 ): GameViewModel(model, playerAvatarId, model.opponent.avatar) {
     val nonPlayerCharacter = NonPlayerCharacter(board, model.opponent)
 
+    init {
+        if (!isPlayerTurn) {
+            playNpcWord()
+        }
+    }
+
     override fun onPlayButtonClick(): Boolean {
         if (super.onPlayButtonClick()) {
             isPlayerTurn = false
-
-            val npcWord = nonPlayerCharacter.findWordToPlay()
-            if (npcWord != null) {
-                GlobalScope.launch {
-                    delay(Random.nextLong(from = 2000L, until = 3500L))
-
-                    board.word.withDrawPathTweenDuration(millis = npcWord.tiles.size * 350) {
-                        for (tile in npcWord.tiles) {
-                            board.word.onSelectTile(tile, Game.Player.PLAYER_2)
-                        }
-
-                        delay(4000L)
-                    }
-
-                    board.playWord(Game.Player.PLAYER_2)
-                    isPlayerTurn = true
-                    updateScore()
-                }
-            }
+            playNpcWord()
             return true
         } else {
             return false
+        }
+    }
+
+    fun playNpcWord() {
+        val npcWord = nonPlayerCharacter.findWordToPlay()
+        if (npcWord != null) {
+            GlobalScope.launch {
+                delay(Random.nextLong(from = 2000L, until = 3500L))
+
+                board.word.withDrawPathTweenDuration(millis = npcWord.tiles.size * 350) {
+                    for (tile in npcWord.tiles) {
+                        board.word.onSelectTile(tile, Game.Player.PLAYER_2)
+                    }
+
+                    delay(4000L)
+                }
+
+                board.playWord(Game.Player.PLAYER_2)
+                isPlayerTurn = true
+                updateScore()
+            }
         }
     }
 }
