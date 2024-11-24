65dc32f005a0b385dc2a7ce85140a29cf54f665b
rbstarbuck <rbstarbuck@gmail.com>
2024-11-24 09:34:48 -0500

    Automatically launch avatar chooser in free main view if not yet chosen

--- app/src/free/java/com/example/worditory/MainViewModel.kt
+++ app/src/free/java/com/example/worditory/MainViewModel.kt
@@ -1,12 +1,23 @@
 package com.example.worditory
 
 import android.content.Context
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.example.worditory.promo.LivePromotionViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 internal class MainViewModel(
     navController: NavController,
     context: Context
 ): MainViewModelBase(navController, context) {
     val livePromo = LivePromotionViewModel()
+
+    init {
+        viewModelScope.launch {
+            if (context.getPlayerAvatarId().first() == 0) {
+                avatarChooser.enabled = true
+            }
+        }
+    }
 }
