6018b65715792798e4147f04a833d29d2c7559e1
rbstarbuck <rbstarbuck@gmail.com>
2024-11-29 08:44:10 -0500

    File rename for consistency between free and live versions

--- app/src/free/java/com/example/worditory/FreeDataStoreKey.kt
+++ app/src/free/java/com/example/worditory/FreeDataStoreKey.kt
@@ -7,15 +7,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 internal fun Context.hasShownLivePromo(): Flow<Boolean> =
-    dataStore.data.map { preferences -> preferences[DataStoreKeyFree.HasShownLivePromo] == true }
+    dataStore.data.map { preferences -> preferences[FreeDataStoreKey.HasShownLivePromo] == true }
 
 internal suspend fun Context.setHasShownLivePromo() {
     dataStore.edit { settings ->
-        settings[DataStoreKeyFree.HasShownLivePromo] = true
+        settings[FreeDataStoreKey.HasShownLivePromo] = true
     }
 }
 
-internal class DataStoreKeyFree {
+internal class FreeDataStoreKey {
 
     companion object {
         internal val HasShownLivePromo = booleanPreferencesKey("hasShownLivePromo")
