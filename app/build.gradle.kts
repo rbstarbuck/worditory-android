042a7dd3ac2ca8ccbb5aac4207421fd0bb3d66ed
rbstarbuck <rbstarbuck@gmail.com>
2024-12-17 12:12:20 -0500

    Bump live version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
             dimension = "version"
             applicationIdSuffix = ".live"
             versionNameSuffix = "-live"
-            versionCode = 2
-            versionName = "1.1"
+            versionCode = 3
+            versionName = "1.2"
         }
     }
     compileOptions {
